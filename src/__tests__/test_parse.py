56a5fa79c9a098178ca4089799506688ed9c5471
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-01-07 02:48:43 -0500

    wip: some test boilerplate

--- src/__tests__/test_parse.py
+++ src/__tests__/test_parse.py
@@ -1,3 +1,17 @@
 """
 Test parse module
 """
+from src.parse import init
+
+
+class TestParse:
+    """
+    Parse test
+    """
+    parser = init()
+
+    def test_path_is_required(self):
+        """
+        Test parser requires the path argument
+        """
+        self.parser.parse_args(args=["-O", "executable"])
